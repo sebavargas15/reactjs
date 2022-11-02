7213df899bcf91dff32c6f628491596c0cc26ba0
sebavargas15 <carlos.vargas@estudiantes.utec.edu.uy>
2022-11-02 19:23:20 -0300

    component Card

--- Vargas-ecomerce/src/App.jsx
+++ Vargas-ecomerce/src/App.jsx
@@ -12,10 +12,27 @@ function Titles(props) {
   );
 }
 
+function Card(props) {
+  return (
+    <div>
+      <img src={props.img} alt="imagen" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      <p>{props.price}</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="div-title">
-      <Titles title='Pet-Ecommerce' />
+      <Titles title="Pet-Ecommerce" />
+
+      <div>
+        <Card img={reactLogo} title="Hueso" description="Hueso para perros" price="100"/>
+        <Card img={reactLogo} title="Camita" description="Cama de algodon" price="700"/>
+        <Card img={reactLogo} title="Racion" description="Racion para gatos" price="1000"/>
+      </div>
     </div>
   );
 }
